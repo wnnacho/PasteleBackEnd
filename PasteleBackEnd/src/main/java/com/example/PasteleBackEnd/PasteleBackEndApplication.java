331b84c647c753d64822b5a5948489ba9d36cd58
wnnacho <ign.alvarezc@duocuc.cl>
2025-11-24 13:29:28 -0300

    Backend vacio
    (añadido backend base sobre el cual trabajar, esta completamente vacio y solo tiene las dependencias)

---
+++ PasteleBackEnd/src/main/java/com/example/PasteleBackEnd/PasteleBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.PasteleBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleBackEndApplication.class, args);
+	}
+
+}
